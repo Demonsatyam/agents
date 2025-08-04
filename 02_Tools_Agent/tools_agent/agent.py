9cf92c31b0d0731ec708aee47d988464da4126fb
Demonsatyam <kumarsatyam01303@gmail.com>
2025-08-04 22:38:22 +0530

    added tools to agent

--- 02_Tools_Agent/tools_agent/agent.py
+++ 02_Tools_Agent/tools_agent/agent.py
@@ -1,12 +1,32 @@
 from google.adk.agents import Agent
 from google.adk.tools import google_search
+from datetime import datetime
+import pytz
 
+
+
+
+
+
+# function that tells the agent to return the current time, date (DD-MM-YYYY), and timezone
+def get_current_time():
+
+    timezone = pytz.timezone("UTC")
+    current_time = datetime.now(timezone)
+    return current_time.strftime("%H:%M:%S"), current_time.strftime("%d-%m-%Y"), str(timezone)
+
+
+
+
+
+# root agent definition
 root_agent = Agent(
     name="tools_agent",
     model="gemini-2.5-flash-lite",
     description="adding up tools to the agents",
     instruction="""
-        
+    you are a helpful agent that can use tools to assist with tasks:
+    google_search: use this tool to search the web for information.
     """,
 
     tools=[google_search],
